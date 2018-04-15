4d37b348f9f805e50c99526cccf8d8511141a023
obx88 <33556172+obx88@users.noreply.github.com>
2018-04-15 20:24:33 +0200

    Added temporary database to import

---
+++ temp/posty.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 15 Kwi 2018, 20:23
+-- Wersja serwera: 10.1.28-MariaDB
+-- Wersja PHP: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `reagujposty`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `posty`
+--
+
+CREATE TABLE `posty` (
+  `ID` int(11) NOT NULL,
+  `Title` tinytext COLLATE utf8_polish_ci NOT NULL,
+  `Content` text COLLATE utf8_polish_ci NOT NULL,
+  `Date` datetime NOT NULL,
+  `Autor` tinytext COLLATE utf8_polish_ci NOT NULL,
+  `imgFileName` tinytext COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `posty`
+--
+
+INSERT INTO `posty` (`ID`, `Title`, `Content`, `Date`, `Autor`, `imgFileName`) VALUES
+(1, 'Prelekcja w zadupiu małym', 'Tak było :p', '2018-04-15 15:34:36', 'Ja', ''),
+(2, 'drugi post xD', 'Znowu tak było lol', '2018-04-15 15:34:36', 'Też ja', '');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `posty`
+--
+ALTER TABLE `posty`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `posty`
+--
+ALTER TABLE `posty`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
