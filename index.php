5dfd517dd7dc8ce6a5995adc13c5448ec64e44d7
obx88 <33556172+obx88@users.noreply.github.com>
2018-06-14 22:43:40 +0200

    Changed title and added analytics

--- index.php
+++ index.php
@@ -34,8 +34,19 @@
 <html lang="pl">
 
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120627207-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-120627207-1');
+</script>
+
+
 	<meta charset="utf-8"/>
-	<title>Aktualności - #reaguj</title>
+	<title>Strona główna - #reaguj</title>
 
 	<link rel="stylesheet" href="css/common.css">
 	<link rel="stylesheet" href="css/Home.css">
