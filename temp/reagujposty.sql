872ab87b55adcc4b4145bc5d1f606dd1634ffb4c
alenart9 <33577025+alenart9@users.noreply.github.com>
2018-10-11 18:22:42 +0200

    Updated DB
    Oke

--- temp/reagujposty.sql
+++ temp/reagujposty.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 06 Wrz 2018, 19:18
--- Wersja serwera: 10.1.28-MariaDB
--- Wersja PHP: 7.1.11
+-- Czas generowania: 11 Paź 2018, 18:19
+-- Wersja serwera: 10.1.35-MariaDB
+-- Wersja PHP: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,20 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `reagujposty`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `howtohelp`
+--
+
+CREATE TABLE `howtohelp` (
+  `id` int(11) NOT NULL,
+  `title` tinytext COLLATE utf8_polish_ci NOT NULL,
+  `shortDesc` text COLLATE utf8_polish_ci NOT NULL,
+  `longDesc` text COLLATE utf8_polish_ci NOT NULL,
+  `img` text COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 -- --------------------------------------------------------
 
@@ -45,25 +59,63 @@ INSERT INTO `posty` (`ID`, `Title`, `Content`, `Date`, `Autor`, `imgFileName`) V
 (1, 'Prelekcja w zadupiu małym', 'Tak było :p', '2018-04-15 15:34:36', 'Ja', 'test.jpg'),
 (2, 'drugi post xD', 'Znowu tak było lol', '2018-04-15 15:34:36', 'Też ja', 'test2.jpg');
 
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `user` tinytext COLLATE utf8_polish_ci NOT NULL,
+  `password` tinytext COLLATE utf8_polish_ci NOT NULL,
+  `firstName` tinytext COLLATE utf8_polish_ci NOT NULL,
+  `surname` tinytext COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
 --
 -- Indeksy dla zrzutów tabel
 --
 
 --
--- Indexes for table `posty`
+-- Indeksy dla tabeli `howtohelp`
+--
+ALTER TABLE `howtohelp`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `posty`
 --
 ALTER TABLE `posty`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT dla tabeli `howtohelp`
+--
+ALTER TABLE `howtohelp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT dla tabeli `posty`
 --
 ALTER TABLE `posty`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
