23be6721e749257cac554ed6e2903cb7e8c8c77d
obx88 <33556172+obx88@users.noreply.github.com>
2019-01-31 23:02:39 +0100

    Changed titles responsive style

--- index.php
+++ index.php
@@ -102,7 +102,7 @@
 				</div>
 			</div>
 			<div class="slide" id="YtSlide">
-				<div class="slideTitle"><span class="underline">Zobacz jak można #reagować</span></div>
+				<div class="slideTitle"><span class="underline">Zobacz jak #reagować</span></div>
 				<div class="flexContainer flexInsideSlide">
 					<div Class="textContainer Red">Nie lękajmy się pomagać innym. To nie jest wcale skomplikowane, a może uratować czyjeś życie. Spójrzmy na przykładowe zachowanie podczas pierwszej pomocy:</div>
 					<iframe class="ytVideo" width="1120" src="https://www.youtube.com/embed/jdbjUnGgYJs?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
