65a2eafc1c43bb42d66d09dcfa51cf5a3dbe215c
obx88 <33556172+obx88@users.noreply.github.com>
2019-02-09 15:50:16 +0100

    some tests

---
+++ nav.php
@@ -0,0 +1,21 @@
+<nav id="StickyMenu">
+
+  <div id="logo">
+    <a href="index.php">
+      #reaguj
+    </a>
+  </div>
+
+  <label for="toggle">&#9776;</label>
+  <input type="checkbox" id="toggle"/>
+
+    <div class="Options">
+      <a href="index.php" id="home">Strona główna</a>
+      <a href="#">Jak reagować</a>
+      <a href="#" class="Hidden">Nasze prelekcje</a>
+      <a href="contact.html">Kontakt</a>
+      <a href="#" class="Hidden">Aktualności</a>
+      <a href="#">Tak o nas mówią</a>
+      <a href="aboutUs.html">O nas</a>
+  </div>
+</nav>
---
+++ template.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+
+<head>
+	<meta charset="utf-8"/>
+	<title>Aktualności - #reaguj</title>
+	<meta name="description" content="Aktualności dotyczące naszej kampanii. Tutaj dowiesz się co się u nas dzieje ;)" />
+	<meta name="keywords" content="pierwsza pomoc, aktualności, newsy,#reaguj" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<link rel="icon" type="image/png" href="favicon.png">
+
+	<link rel="stylesheet" href="css/Home.css">
+	<link rel="stylesheet" href="css/common.css">
+	<link rel="stylesheet" href="css/news.css">
+	<link rel="stylesheet" href="css/scrollbar.css">
+
+</head>
+
+<body>
+	<?php
+		require "nav.php";
+	?>
+
+</body>
+
+</html>
