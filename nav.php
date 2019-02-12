55db8bdf71ef50071a381e1019e190b0efe3d30e
obx88 <33556172+obx88@users.noreply.github.com>
2019-02-12 23:14:20 +0100

    There will be dragons

--- nav.php
+++ nav.php
@@ -6,15 +6,18 @@
     </a>
   </div>
 
-  <label for="toggle">&#9776;</label>
+  <label id="hamburger">&#9776;</label>
 
     <div class="Options">
       <a href="index.php" id="home">Strona główna</a>
       <a href="howToHelp.php">Jak reagować</a>
-      <a href="#" class="Hidden">Nasze prelekcje</a>
+      <!--<a href="#" class="Hidden">Nasze prelekcje</a>-->
       <a href="contact.php">Kontakt</a>
-      <a href="#" class="Hidden">Aktualności</a>
+      <!--<a href="#" class="Hidden">Aktualności</a>-->
       <a href="#">Tak o nas mówią</a>
       <a href="aboutUs.php">O nas</a>
+
+      <script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
+      <script src="js/menu.js" charset="utf-8"></script>
   </div>
 </nav>
