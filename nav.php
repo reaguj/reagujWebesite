c6fc0e0b5871dc03b8403655fdda2735e828c5ab
obx88 <33556172+obx88@users.noreply.github.com>
2019-02-13 20:22:44 +0100

    Updated Navbar

--- nav.php
+++ nav.php
@@ -14,7 +14,7 @@
       <!--<a href="#" class="Hidden">Nasze prelekcje</a>-->
       <a href="contact.php">Kontakt</a>
       <!--<a href="#" class="Hidden">Aktualności</a>-->
-      <a href="#">Tak o nas mówią</a>
+      <a href="talkAboutUs.php">Tak o nas mówią</a>
       <a href="aboutUs.php">O nas</a>
 
       <script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
