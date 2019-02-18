2c9766da48fbcaa0ad23a81f9b9734cdcc1666df
obx88 <33556172+obx88@users.noreply.github.com>
2019-02-18 23:33:15 +0100

    added footer

--- index.php
+++ index.php
@@ -147,29 +147,9 @@
 				</div>
 			</div>
 	</div>
-	<footer>
-		<div class="icons">
-			<div class="contactTile">
-				<i class="fab fa-instagram"></i>
-			</div>
-			<div class="contactTile">
-				<i class="fab fa-snapchat-square"></i>
-			</div>
-			<div class="contactTile">
-				<i class="fab fa-youtube"></i>
-			</div>
-			<div class="contactTile">
-				<i class="fas fa-comment-dots"></i>
-			</div>
-			<div class="contactTile">
-				<i class="fab fa-facebook-square"></i>
-			</div>
-		</div>
-		<div class="mail">kontakt.reaguj@gmail.com</div>
-		<div class="copyright">
-			&copy; 2018 #reaguj. Wszystkie prawa zastrzeżone
-		</div>
-	</div>
+<?php
+	require "footer.php"
+ ?>
 
 </body>
 
