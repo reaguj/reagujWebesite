4e66e3c13921a7f63ecfe3a9245023f163be5554
obx88 <33556172+obx88@users.noreply.github.com>
2019-02-21 14:41:09 +0100

    bugfix

--- const/connect.php
+++ const/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-	$host="https://az-serwer1838071.online.pro";
+	$host="az-serwer1838071.online.pro";
 	$db_user="00140028_blogposts";
 	$db_password="ZAQ!2wsxCDE#4rfv";
 	$db_name="00140028_blogposts";
